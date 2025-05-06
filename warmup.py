9884bf010731029cb6332a73f30358284227e3b9
chiara yakob <chiaray@kth.se>
2025-05-06 10:52:39 +0200

    task 1 filen

---
+++ warmup.py
@@ -0,0 +1,12 @@
+import pylab as pb
+import numpy as np
+import matplotlib . pyplot as plt
+from math import pi
+from scipy . stats import multivariate_normal
+from scipy . spatial . distance import cdist
+# To draw n samples from multivariate Gaussian distribution with mu and Cov:
+f = np . random . multivariate_normal ( mu , Cov , n )
+
+
+#TASK 1
+
